677e3c35d4462d814563208d9fafa0513f512061
edsonGmuniz <edsonmunizz@hotmail.com>
2023-11-18 14:59:14 -0300

    super mario

---
+++ script.js
@@ -0,0 +1,11 @@
+let formulario = document.querySelector(".fale-conosco")
+let mascara = document.querySelector(".mascara-form")
+
+function cliqueiNoBotao(){
+    formulario.style.left = "500px"
+    mascara.style.visibility = "visible"
+}
+
+function sumirFormulario(){
+    formulario.style.left = "315px"
+}
